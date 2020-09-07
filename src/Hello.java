47222b48cddc773d12f113f097f69e0a68a3c032
Andreas-Ravn <andr8343@edu.easj.dk>
2020-09-07 11:37:09 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello World!");
+        System.out.println("Who goes there!");
+        System.out.println("What's That noise!");
+        System.out.println("I hear some one prowling around here");
+        System.out.println(5);
+    }
+}
